e3dfb5e41c0504e8edd0981c7b919a3be200cb70
Greg Whitworth <gwhit@microsoft.com>
2016-10-17 13:46:05 -0700

    Removed gathering attribute values in preperation of recipes

--- cssUsage.src.js
+++ cssUsage.src.js
@@ -455,45 +455,15 @@ void function() {
     }
 
     function GetAttributes(element, node) {
-        // The attributes that we want to keep values for
-        var whitelist = [
-                        "aria-hidden",
-                        "aria-live",
-                        "aria-atomic",
-                        "aria-busy",
-                        "aria-disabled",
-                        "aria-expanded",
-                        "aria-sort",
-                        "aria-readonly",
-                        "aria-required",
-                        "aria-selected",
-                        "type",
-                        "dir",
-                        "disabled",
-                        "draggable",
-                        "lang",
-                        "role",
-                        "content",
-                        "name"
-                        ];
-
         for(var i = 0; i < element.attributes.length; i++) {
             var att = element.attributes[i];
 
             // Only keep attributes that do not contain a dash unless they're in the whitelist
-            if(att.nodeName.indexOf('-') == -1 || whitelist.indexOf('aria-') > -1) {
+            if(att.nodeName.indexOf('-') == -1) {
 
                 var attributes = HtmlUsageResults.attributes || (HtmlUsageResults.attributes = {});
                 var attributeTag = attributes[node] || (attributes[node] = {});
-                var attribute = attributeTag[att.nodeName] || (attributeTag[att.nodeName] = { count: 0, values: {}});        
-                
-
-                if (whitelist.indexOf(att.nodeName) > -1 || (att.nodeName == "name" && node == "META")) {
-                    var attributeValue = attribute.values[att.value];
-
-                    attribute.values[att.value] = 1;
-                }
-                
+                var attribute = attributeTag[att.nodeName] || (attributeTag[att.nodeName] = { count: 0, values: {}});                
                 attribute.count++;
             }
         }
