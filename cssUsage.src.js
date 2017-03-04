045b9118261b9a76d68c5005014ad003839a3e25
Mustapha Jaber <meksh@hotmail.com>
2017-03-04 09:26:54 -0800

    Fixed a bug in mediaelements.js with access to undefined attribute

--- cssUsage.src.js
+++ cssUsage.src.js
@@ -1611,32 +1611,34 @@ void function() {
 
         if (element.nodeName == "A")
         {
-            var host = element.attributes["href"].value;
+            if (element.attributes["href"] !== undefined) {
+                var host = element.attributes["href"].value;
 
-            if (host.match("get.adobe.com") || host.match("get2.adobe.com")) {
-                if (host.match("flash")) {
-                    // url is get.adobe.com/*flash*
-                    isFlashDownloadLink = true;
-                }
-            }
-            else if (host.match("adobe.com") || host.match("macromedia.com") ||
-                     host.match("www.adobe.com") || host.match("www.macromedia.com")) {
-                var token = host.match("/go/");
-                if (token != null) {
-                    token = token.match("get");
-                    if (token != null) {
-                        token = token.match("flash");
-                        if (token != null) {
-                            // url is (www).adobe.com/*/go/*get*flash* or (www).macromedia.com/*/go/*get*flash*
-                            isFlashDownloadLink = true;
-                        }
+                if (host.match("get.adobe.com") || host.match("get2.adobe.com")) {
+                    if (host.match("flash")) {
+                        // url is get.adobe.com/*flash*
+                        isFlashDownloadLink = true;
                     }
                 }
-                else if (host.match("/shockwave/download/") &&
-                         host.match("download.cgi") || host.match("index.cgi")) {
-                    if (host.match("?P1_Prod_Version=ShockwaveFlash")) {
-                        // url is (www).(adobe || macromedia).com/shockwave/download/(download || index).cgi?P1_Prod_Version=ShockwaveFlash
-                        isFlashDownloadLink = true;
+                else if (host.match("adobe.com") || host.match("macromedia.com") ||
+                         host.match("www.adobe.com") || host.match("www.macromedia.com")) {
+                    var token = host.match("/go/");
+                    if (token != null) {
+                        token = token.match("get");
+                        if (token != null) {
+                            token = token.match("flash");
+                            if (token != null) {
+                                // url is (www).adobe.com/*/go/*get*flash* or (www).macromedia.com/*/go/*get*flash*
+                                isFlashDownloadLink = true;
+                            }
+                        }
+                    }
+                    else if (host.match("/shockwave/download/") &&
+                             host.match("download.cgi") || host.match("index.cgi")) {
+                        if (host.match("?P1_Prod_Version=ShockwaveFlash")) {
+                            // url is (www).(adobe || macromedia).com/shockwave/download/(download || index).cgi?P1_Prod_Version=ShockwaveFlash
+                            isFlashDownloadLink = true;
+                        }
                     }
                 }
             }
