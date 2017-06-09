8f08aeab1e7353420925911336d64120e20df670
Mustapha Jaber <mustjab@windows.microsoft.com>
2017-06-09 15:26:21 -0700

    Added RequestPayment function check recipe

--- Recipe.min.js
+++ Recipe.min.js
@@ -1541,6 +1541,40 @@ void function() { try {
 } catch (ex) { /* do something maybe */ throw ex; } }();
 
 /* 
+    RECIPE: Request Payment
+    -------------------------------------------------------------
+    Author: Mustapha Jaber
+    Description: Find use of RequestPayment in script.
+*/
+
+void function() {
+    window.CSSUsage.StyleWalker.recipesToRun.push( function paymentRequest(/*HTML DOM Element*/ element, results) {
+        var nodeName = element.nodeName;
+        var script = "RequestPayment"
+        if (nodeName == "SCRIPT")
+        {
+            results[nodeName] = results[nodeName] || { count: 0, };
+            // if inline script. ensure that it's not our recipe script and look for string of interest
+            if (element.text !== undefined && element.text.indexOf(script) != -1)
+            {
+                results[nodeName].count++;
+            }
+            else if (element.src !== undefined && element.src != "")
+            {
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", element.src, false);
+                //xhr.setRequestHeader("Content-type", "text/javascript");
+                xhr.send();
+                if (xhr.status === 200 && xhr.responseText.indexOf(script) != -1)
+                {
+                    results[nodeName].count++;
+                }
+            }
+        }
+        return results;
+    });
+}();
+/* 
     RECIPE: z-index on static flex items
     -------------------------------------------------------------
     Author: Francois Remy
