016c4573c2755eb6b39874f0942c64d779fe371e
rmango <morgan-graham@live.com>
2017-07-10 16:48:25 -0700

    Added image search for browsers

---
+++ src/recipes/imgEdgeSearch.js
@@ -0,0 +1,24 @@
+/* 
+    RECIPE: imgEdgeSearch
+    -------------------------------------------------------------
+    Author: Morgan, Lia, Joel, Malick
+    Description: Looking for sites that do not include edge as a supported browser
+*/
+
+void function() {
+    window.CSSUsage.StyleWalker.recipesToRun.push( function imgEdgeSearch( element, results) {
+        //tests for images
+        if(element.nodeName == "IMG") {
+            var browsers = ["internet explorer","ie","firefox","chrome","safari","edge"];
+            for(var i = 0; i < browsers.length; i++) {
+                if(element.getAttribute("alt").toLowerCase().indexOf(browsers[i]) != -1|| element.getAttribute("src").toLowerCase().indexOf(browsers[i]) != -1) {
+                    results[browsers[i]] = results[browsers[i]] || {count: 0};
+                    results[browsers[i]].count++;
+                }
+            
+            }   
+        }
+
+        return results;
+    });
+}();
