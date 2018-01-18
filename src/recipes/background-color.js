2ad167b7a0ca059304a42c8e79d488cf04f4694d
Greg Whitworth <gwhit@microsoft.com>
2018-01-18 11:38:32 -0800

    Wrote background-color prop recipe

---
+++ src/recipes/background-color.js
@@ -0,0 +1,28 @@
+/*
+    RECIPE: BACKGROUND-COLOR
+    -------------------------------------------------------------
+    Author: Greg Whitworth
+    Description: This recipe is solely checking for background-color for
+    data analysis purposes.
+*/
+
+void function() {
+    window.CSSUsage.StyleWalker.recipesToRun.push(function backgroundColorProp(/*HTML DOM Element*/ element, results) {
+
+        // Bail if the element doesn't have the props we're looking for
+        if(!element.CSSUsage || !(element.CSSUsage["background-color"])) return;
+
+        var values = [];
+
+        values = values.concat(element.CSSUsage["background-color"].valuesArray);
+
+        for(var i = 0; i < values.length; i++) {
+            var value = values[i];
+
+            results[value] = results[value] || { count: 0 };
+            results[value].count++;
+        }
+
+        return results;
+    });
+}();
