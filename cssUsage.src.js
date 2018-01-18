a2fed58666c0e038232fa0f8c968045fed17c687
Greg Whitworth <gwhit@microsoft.com>
2018-01-18 11:42:04 -0800

    Added the div counting recipe

--- cssUsage.src.js
+++ cssUsage.src.js
@@ -1569,6 +1569,26 @@ void function() {
     });
 }();
 /*
+    RECIPE: Div Count
+    -------------------------------------------------------------
+    Author: Greg Whitworth
+    Description: This recipe is solely counting divs for
+    data analysis purposes.
+*/
+
+void function() {
+    window.CSSUsage.StyleWalker.recipesToRun.push( function divCount(/*HTML DOM Element*/ element, results) {
+        if(!element.CSSUsage) return;
+
+        if(element.nodeName == "DIV") {
+            results["DIV"] = results["DIV"] || { count: 0 };
+            results["DIV"].count++;
+        }
+
+        return results;
+    });
+}();
+/*
     RECIPE: Metaviewport
     -------------------------------------------------------------
     Author: Greg Whitworth
