99e20922e1a7bc95a6d541b2555803ead0ac661a
Greg Whitworth <gwhit@microsoft.com>
2018-04-30 17:04:39 -0700

    Trying to fix up regex issues

--- Recipe.min.js
+++ Recipe.min.js
@@ -1618,15 +1618,15 @@ void function() {
         return results;
     });
 }();
-/* 
+/*
     RECIPE: unsupported browser
     -------------------------------------------------------------
     Author: Morgan Graham, Lia Hiscock
-    Description: Looking for phrases that tell users that Edge is not supported, or to switch browers. 
+    Description: Looking for phrases that tell users that Edge is not supported, or to switch browers.
 */
 
 void function() {
-    window.CSSUsage.StyleWalker.recipesToRun.push( function unsupportedBrowser( element, results) {        
+    window.CSSUsage.StyleWalker.recipesToRun.push( function unsupportedBrowser( element, results) {
         //tests for phrases
         var switchPhraseString = new RegExp("((?:Switch to|Get|Download|Install)(?:\\w|\\s)+(?:Google|Chrome|Safari|firefox|Opera|Internet Explorer|IE))","i");
         var supportedPhraseString = new RegExp("((?:browser|Edge)(?:\\w|\\s)+(?:isn't|not|no longer)(?:\\w|\\s)+(?:supported|compatible))", "i");
@@ -1634,10 +1634,10 @@ void function() {
                         {str:supportedPhraseString, name:"supportedPhrase"}];;
 
         for(var i = 0; i < needles.length; i++) {
-            var found = element.textContent.match(needles[i].str);            
+            var found = element.textContent.match(needles[i].str);
             if(found) {
                 if(found.length > 0 && found !== (null || undefined)) {
-                    results[needles[i].name] = results[needles[i].name] || {count: 0, match: "", container: ""};
+                    results[needles[i].name] = results[needles[i].name] || {count: 0, match: [], container: ""};
                     results[needles[i].name].count++;
 
                     var parent = element.parentElement;
@@ -1646,13 +1646,18 @@ void function() {
                         var val = outer.replace(element.parentElement.innerHTML, "");
                         results[needles[i].name].container = val;
                     }
-                    
+
                     found = remove(found, " ");
-                    results[needles[i].name].match = found.join();
+
+                    found.forEach(f => {
+                        if(!results[needles[i].name].match.includes(f)) {
+                            results[needles[i].name].match.push(f);
+                        }
+                    });
                 }
             }
         }
-        
+
         return results;
     });
 
